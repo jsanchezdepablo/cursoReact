31a75d3c81936380d8e52157f17cd9f0430e7456
Jesús Sánchez de Pablo <jsdp93@gmail.com>
2019-03-14 08:30:50 +0100

    React con JSX

--- basicosreact/src/App.js
+++ basicosreact/src/App.js
@@ -1,14 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
 class App extends React.Component {
   render() {
-    return React.createElement(
-      "h1", //Etiqueta
-      { id: "titulo", className: "encabezado" }, //Atributos
-      "Hola mundo" //Contenido
+
+    let empleado = {
+      nombre: "Jesús",
+      edad: "189"
+    }
+
+    return(
+      <div>
+        <h1>WHAT</h1>
+        <p>Hola {empleado.nombre}, tienes {empleado.edad} años </p>
+      </div>
     );
   }
 }
 
-export default App;
+export default App; //Exportamos la clase, la cual sera el componente App
